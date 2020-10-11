9b04373e3d240b795d810fcb713aaea4b18e59cc
Robert Puccinelli <robertpuccinelli@users.noreply.github.com>
2020-10-10 19:34:16 -0700

    Add initial Pi 3B bare metal code

---
+++ designs/DLSSensorModule/Code/RPi_DLS.h
@@ -0,0 +1,7 @@
+#include <stdint.h>
+
+uint16_t dataRead(void);
+int dataClk(void);
+void uartInit(void);
+unsigned int uartRead(void);
+void uartSend(unsigned int c);
---
+++ designs/DLSSensorModule/Code/buffer.h
@@ -0,0 +1,3 @@
+//buffer.h
+
+void * buffer(long);
